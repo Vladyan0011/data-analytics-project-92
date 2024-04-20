04c9ce71802477b167c7d08ac055249aba0183b5
Chupshev Vlad <154665035+Vladyan0011@users.noreply.github.com>
2024-04-20 20:29:17 +0400

    Update queries.sql
    Add 3 new queries

--- queries.sql
+++ queries.sql
@@ -54,3 +54,50 @@ GROUP BY seller, day_of_week, day_number
 SELECT seller, day_of_week, income
 FROM tab 
 ORDER BY day_number, seller;
+
+--Group all of customers by age groups
+SELECT
+	CASE
+		WHEN c.age BETWEEN 16 AND 25 THEN '16-25'
+		WHEN c.age BETWEEN 26 AND 40 THEN '26-25'
+		WHEN c.age > 40 THEN '40+'
+	END AS age_category,
+	COUNT(*) AS age_count
+FROM customers AS c
+GROUP BY age_category
+ORDER BY age_category;
+
+-- Group income by month of purchase and unique customers
+SELECT
+	TO_CHAR(sales.sale_date, 'YYYY-MM') AS selling_month,
+	COUNT(DISTINCT sales.customer_id) AS customers,
+	FLOOR(SUM(products.price * sales.quantity)) AS income
+FROM sales
+JOIN products ON products.product_id = sales.product_id 
+GROUP BY selling_month
+ORDER BY selling_month;
+
+--Find all customers who made first purchase with sale products
+WITH customers_purchases AS (
+    SELECT
+    	customers.customer_id,
+    	CONCAT(customers.first_name, ' ', customers.last_name) AS customer,
+    	sales.sale_date,
+    	products.product_id,
+    	products.price,
+    	CONCAT(employees.first_name, ' ', employees.last_name) AS seller,
+    	ROW_NUMBER()
+    		OVER (PARTITION BY customers.customer_id ORDER BY sales.sale_date, products.price) AS row_number
+    	FROM sales
+    	JOIN products ON products.product_id = sales.sales_id 
+    	JOIN customers ON customers.customer_id = sales.customer_id
+    	JOIN employees ON sales.sales_person_id = employees.employee_id
+    	ORDER BY customers.customer_id, sales.sale_date, products.price
+)
+SELECT
+	customer,
+	sale_date,
+	seller
+FROM customers_purchases
+WHERE row_number = 1 AND price = 0
+ORDER BY customer_id;
